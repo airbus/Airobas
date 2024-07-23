010531722e516349817a8d11771be3ec9a0e3cb1
poveda_g <guillaume.poveda@airbus.com>
2024-07-23 11:23:04 +0200

    change test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -5,5 +5,5 @@ def test_imports():
     import gurobipy
     import gurobi_ml
     import maraboupy
-    import verif_pipeline
+    # import verif_pipeline
     from blocks_hub.adv_block import CleverhansAdvBlock, CleverHansMultiIndexAdvBlock

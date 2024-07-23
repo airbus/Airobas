62c4b64ac5b742dcc96bbf9c533d3f595ae6c6a8
poveda_g <guillaume.poveda@airbus.com>
2024-07-23 10:16:11 +0200

    first commit for workflow and unittest

---
+++ tests/test_imports.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+def test_imports():
+    import gurobi
+    import gurobi_ml
+    import maraboupy
+    import verif_pipeline
+    from blocks_hub.adv_block import CleverhansAdvBlock, CleverHansMultiIndexAdvBlock
